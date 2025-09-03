c3924515ee86a5b943e5357933efd3ae589c472f
ITÔ Hiroki <abies.firma@gmail.com>
2025-09-03 09:41:20 +0900

    update prediction map

--- scripts/prediction_map.R
+++ scripts/prediction_map.R
@@ -7,14 +7,21 @@ library(sf)
 library(leaflet)
 
 # load data
-data <- sf::st_read("data/kuma_prediction.geojson") |>
+prob_data <- sf::st_read("data/kuma_prediction.geojson") |>
+  dplyr::mutate(prob = est * 100) |>   # percent
   sf::st_transform(crs = "WGS84")
 
 # map
 leaflet() |>
   addTiles() |>
-  setView(lng = 136.7, lat = 37.0, zoom = 10) |>
+  setView(lng = 136.7, lat = 36.6, zoom = 11) |>
   addPolygons(data = data,
-              fillColor = colorNumeric("YlOrRd", domain = c(0, 1))(data$est),
-              fillOpacity = 0.7,
-              stroke = FALSE)
+              fillColor = colorNumeric("YlOrRd", domain = c(0, 100))(prob_data$prob),
+              fillOpacity = 0.6,
+              stroke = FALSE,
+              popup = sprintf("%2.1f%%", prob_data$prob)) |>
+  addLegend(position = "topright",
+            pal = colorNumeric("YlOrRd", domain = c(0, 100)),
+            values = c(0, 100),
+            title = "確率(%)",
+            opacity = 1)
